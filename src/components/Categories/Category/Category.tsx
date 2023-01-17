535578db7c8888f64568904a93da7ace830b6dc7
Denis <ankalogan@gmail.com>
2023-01-17 16:32:48 +0300

    feature: implemented accordion menu for categories

--- src/components/Categories/Category/Category.tsx
+++ src/components/Categories/Category/Category.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from './Category.module.scss'
 
 type CategoryProps = {
@@ -6,10 +6,25 @@ type CategoryProps = {
     icon: string
 }
 
-export const Category:FC<CategoryProps> = ({name, icon}) => {
+export const Category: FC<CategoryProps> = ({ name, icon }) => {
+    const [isVisible, setIsVisible] = useState(true)
+    const onDivCLickHandler = () => {
+        setIsVisible(!isVisible)
+    }
     return (
-            <div className={styles.category}>
+        <div className={styles.categoryContainer}>
+            <div className={styles.categoryTitle} onClick={onDivCLickHandler}>
                 <span>{name}</span><span> {'>'} </span>
             </div>
+            {isVisible && <div className={styles.subCategory}>
+                <div >
+                    Premier Leaugue
+                </div>
+                <div>La League</div>
+                <div>Bundesliga</div>
+                <div>Seria A</div>
+            </div>
+            }
+        </ div>
     )
 }
