b223b337127ec45fcb26349cc7f7cb7e4c70f282
Denis <ankalogan@gmail.com>
2023-01-29 16:40:35 +0300

    refactor: data taken from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
 import { Header } from './components/Header/Header';
 import { Login } from './pages/Login/Login';
 import { MainPage } from './pages/main/MainPage';
 import { Results } from './pages/results/Results';
+import { RootStateType } from './store';
+import { useSelector } from 'react-redux';
+import { AuthState, setIsLoggedIn } from './store/auth-reducer';
+import { useAppSelector } from './hooks/useAppSelector';
 
 export const App = () => {
-  const [isLoggedIn, setIsLoggenIn] = useState(false)
-  const onClickHandler = () => {
-    setIsLoggenIn(!isLoggedIn)
-  }
+  const {isLoggedIn} = useAppSelector<AuthState>(state => state.auth)
 
   return (
     <div>
-      <Header isLoggedIn={isLoggedIn} onClickHandler={onClickHandler} />
+      <Header />
 
       {
         isLoggedIn ?
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,20 @@
 import { FC, useState } from 'react'
+import { useAppDispatch } from '../../hooks/useAppDispatch'
+import { useAppSelector } from '../../hooks/useAppSelector'
+import { RootStateType } from '../../store'
+import { setIsLoggedIn } from '../../store/auth-reducer'
 import styles from './Header.module.scss'
 
 interface HeaderPropsType {
-    isLoggedIn: boolean
-    onClickHandler: () => void
 
 }
 
-export const Header: FC<HeaderPropsType> = (props) => {
-    const {isLoggedIn, onClickHandler } = props
+export const Header: FC<HeaderPropsType> = () => {
+    const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
+    const dispatch = useAppDispatch()
+    const onClickHandler = () => {
+      dispatch(setIsLoggedIn(!isLoggedIn))
+    }
 
     return (
         <header className={styles.header}>
