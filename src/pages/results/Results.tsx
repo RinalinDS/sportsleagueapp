15cbf7900e9f44fbc5f1eb379416df4e88032816
Denis <ankalogan@gmail.com>
2023-02-02 16:12:17 +0300

    feature: matchday component

--- src/pages/results/Results.tsx
+++ src/pages/results/Results.tsx
@@ -1,25 +1,28 @@
+import { MatchdayAccordion } from '../../components/MatchdayAccordion'
 import styles from './Results.module.scss'
+
+const data = [
+    { homeTeam: 'Arsenal', roadTeam: 'Chelsea', homeTeamScore: 0, roadTeamScore: 0 },
+    { homeTeam: 'Leeds', roadTeam: 'Leicester', homeTeamScore: 0, roadTeamScore: 2 },
+    { homeTeam: 'Man City', roadTeam: 'Blackburn', homeTeamScore: 3, roadTeamScore: 1 },
+]
 
 export const Results = () => {
     return (
         <div className={styles.resultsPage}>
             <div className={styles.container}>
                 <div className={styles.tableContainer}>
-                    <div className={styles.grid}>
-                        <div className={styles.team}>Arsenal</div>
-                        <div className={styles.score}>0 - 0 </div>
-                        <div className={styles.team}>Chelsea</div>
-                    </div>
-                    <div className={styles.grid}>
-                        <div className={styles.team}>Leeds</div>
-                        <div className={styles.score}>0 - 2 </div>
-                        <div className={styles.team}>Leicester</div>
-                    </div>
-                    <div className={styles.grid}>
-                        <div className={styles.team}>Man City</div>
-                        <div className={styles.score}>3 - 0 </div>
-                        <div className={styles.team}>Blackburn</div>
-                    </div>
+                    {data.map((day, index) => {
+                        return (
+                            <MatchdayAccordion isVisible={index === 0} title={`Matchday ${index + 1}`}>
+                                <div className={styles.grid}>
+                                    <div className={styles.team}>{day.homeTeam}</div>
+                                    <div className={styles.score}>{day.homeTeamScore} - {day.roadTeamScore} </div>
+                                    <div className={styles.team}>{day.roadTeam}</div>
+                                </div>
+                            </MatchdayAccordion>
+                        )
+                    })}
                 </div>
             </div>
         </div>
