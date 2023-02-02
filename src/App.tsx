441adcd8af4a2ce3256c39a697af515bd4affa70
Denis <ankalogan@gmail.com>
2023-02-02 16:09:04 +0300

    feature:  routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import { Header } from './components/Header/Header';
-import { Login } from './pages/Login/Login';
-import { MainPage } from './pages/main/MainPage';
-import { Results } from './pages/results/Results';
-import { RootStateType } from './store';
-import { useSelector } from 'react-redux';
-import { AuthState, setIsLoggedIn } from './store/auth-reducer';
-import { useAppSelector } from './hooks/useAppSelector';
+import { RoutesComponent } from './components/Routes';
 
 export const App = () => {
-  const {isLoggedIn} = useAppSelector<AuthState>(state => state.auth)
-
   return (
     <div>
       <Header />
-
-      {
-        isLoggedIn ?
-          <>
-            <MainPage />
-            <Results />
-          </>
-          :
-          <Login />
-      }
+      <RoutesComponent />
     </div>
   );
 }
---
+++ src/enum/Path.ts
@@ -0,0 +1,6 @@
+export enum Path {
+    Main = '/',
+    Results = '/results',
+    Login = '/login',
+    Any = '*'
+}
