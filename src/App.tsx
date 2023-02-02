1c6c786452e118b4f0380084b10feccf7347db49
Denis <ankalogan@gmail.com>
2023-02-02 20:05:16 +0300

    feature: login check in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react';
 import { Header } from './components/Header/Header';
 import { RoutesComponent } from './components/Routes';
+import { useAppDispatch } from './hooks/useAppDispatch';
+import { setIsLoggedIn } from './store/auth-reducer';
 
 export const App = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const flag = localStorage.getItem('isLoggedIn')
+    flag && dispatch(setIsLoggedIn(true))
+  }, [])
+
   return (
     <div>
       <Header />
