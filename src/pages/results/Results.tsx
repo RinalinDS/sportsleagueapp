2f6ec0ef923b29b59e4714462ce9ce6f883b34e4
DenisPilyutin <denispilyutin@gmail.com>
2023-02-18 22:36:18 +0200

    refactor: extend from common pageContainer

--- src/pages/results/Results.tsx
+++ src/pages/results/Results.tsx
@@ -1,13 +1,7 @@
 import styles from './Results.module.scss'
-import {useAppSelector} from "../../hooks/useAppSelector";
-import {ResultsType} from "../../models/ResultModel";
-import {useEffect, useState} from "react";
-import {useAppDispatch} from "../../hooks/useAppDispatch";
-import {setLatestResults} from "../../store/results-reducer";
 import ResultsTable from "../../components/ResultsTable/ResultsTable";
-import {data} from "../../utils/mockData";
 import {MatchdayAccordion} from "../../components/MatchdayAccordion/MatchdayAccordion";
-import { useLatestResultsKeys } from '../../hooks/useLatestResultsKeys';
+import {useLatestResultsKeys} from '../../hooks/useLatestResultsKeys';
 
 export const Results = () => {
     const {keys, latestResults} = useLatestResultsKeys()
@@ -15,13 +9,11 @@ export const Results = () => {
     return (
         <div className={styles.resultsPage}>
             <div className={styles.container}>
-                <div className={styles.tableContainer}>
-                    {keys.map((dataKey, index) =>
-                        <MatchdayAccordion key={dataKey} isVisible={index === 0} title={dataKey.trim()}>
-                            <ResultsTable latestResult={latestResults[dataKey]}/>
-                        </MatchdayAccordion>
-                    )}
-                </div>
+                {keys.map((dataKey, index) =>
+                    <MatchdayAccordion key={dataKey} isVisible={index === 0} title={dataKey.trim()}>
+                        <ResultsTable latestResult={latestResults[dataKey]}/>
+                    </MatchdayAccordion>
+                )}
             </div>
         </div>
     )
